d4faa79716b2c326e1c44f9d54ebbf5ef4ba2343
Prometheusz2020 <pratali1@hotmail.com>
2026-07-20 11:26:25 -0300

    chore: clean up scratch scripts from workspace root

--- scratch_test_config.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-console.log('FB_USER:', JSON.stringify(process.env.FB_USER));
-console.log('FB_PASSWORD:', JSON.stringify(process.env.FB_PASSWORD));
-console.log('FB_DATABASE:', JSON.stringify(process.env.FB_DATABASE));
